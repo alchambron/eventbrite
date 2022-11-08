3f964d1196a2b898e328ce9156b8a7f9b6eb5503
alchambron <alexis.chambron@gmail.com>
2022-11-08 17:46:20 +0100

    Creating and Completing Show Event Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
   resources :attendances
   resources :events
   root "events#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:show]
 end
